8ac406aaf54a69027adecadf27743388c389f3d7
tkf2019 <kaifu6821@qq.com>
2021-06-06 22:49:46 +0800

    Generate new imgs

--- src/run.sh
+++ src/run.sh
@@ -12,7 +12,7 @@
 # python main.py -m RNN -b 64 --mode test --hidden 128 >>../logs/RNN.log
 python main.py -m RNN -b 64 --mode train --hidden 128 --dropout 0.3 --patience 7 >>../logs/RNN.log
 python main.py -m RNN -b 64 --mode test --hidden 128 >>../logs/RNN.log
-python main.py -m MLP -b 32 --mode train --hidden 512 --dropout 0 >>../logs/MLP.log
-python main.py -m MLP -b 32 --mode test --hidden 512 >>../logs/MLP.log 
+python main.py -m MLP -b 32 --mode train --hidden 1024 --dropout 0 >>../logs/MLP.log
+python main.py -m MLP -b 32 --mode test --hidden 1024 >>../logs/MLP.log 
 
 # tree -I "*.pyc|*.log|__pycache__|__MACOSX|tree" -n -o tree
